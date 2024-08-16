0ec9de16284a637a0d940f3f853b27050ff2e82b
carlstong <carlstongrefalde@gmail.com>
2024-08-16 22:20:51 +0800

    Add project files.

---
+++ BaseStoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaseStoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ BaseStoreMVC/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations; //added
+using System.ComponentModel.DataAnnotations.Schema; //added
+
+namespace BaseStoreMVC.Models
+{
+    public class Transaction
+    {
+        [Key] //added
+        public int TransactionId { get; set; }
+        //added 
+        [Column(TypeName ="nvarchar(12)")]
+        public string AccountNum { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string BeneficiaryName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string BankName { get; set;}
+        [Column(TypeName = "nvarchar(11)")]
+        public string SWIFTCODE { get; set; }
+     
+        public int Amount { get; set; }
+  
+        public DateTime Date { get; set; }
+    }
+}
